6451c57ff6d4a6eb5a53dbf6b08fce9ec87a120d
Cai Chi <mildrage01@gmail.com>
2024-09-18 15:32:50 +0800

    Implemented basic expression evaluation function

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -21,10 +21,7 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
-
-  /* TODO: Add more token types */
-
+  TK_NOTYPE = 256, TK_NUM, TK_EQ, TK_MULTIPLY, TK_DIVIDE, TK_PLUS, TK_MINUS, TK_PARL, TK_PARR
 };
 
 static struct rule {
@@ -36,9 +33,16 @@ static struct rule {
    * Pay attention to the precedence level of different rules.
    */
 
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ},        // equal
+  {" +",  TK_NOTYPE},              // spaces
+  {"(0|[1-9][0-9]*)", TK_NUM},     // numbers
+  {"==",  TK_EQ},                  // equal
+  {"\\*", TK_MULTIPLY},            // multiply
+  {"/",   TK_DIVIDE},              // divide
+  {"\\+", TK_PLUS},                // plus
+  {"\\-", TK_MINUS},               // minus
+  {"\\(", TK_PARL},                // left parenthesis
+  {"\\)", TK_PARR},                // right parenthesis
+
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -67,14 +71,14 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
+static Token tokens[65536] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
+static bool legal_flag __attribute__((used)) = 1;
 
 static bool make_token(char *e) {
   int position = 0;
   int i;
   regmatch_t pmatch;
-
   nr_token = 0;
 
   while (e[position] != '\0') {
@@ -86,18 +90,33 @@ static bool make_token(char *e) {
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
-
         position += substr_len;
-
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
-        switch (rules[i].token_type) {
-          default: TODO();
+        tokens[nr_token].type = 0;
+        for (int i = 0; i < 32; i++)
+        {
+          tokens[nr_token].str[i] = '\0';
         }
-
+        
+        switch (rules[i].token_type) {
+          case TK_NOTYPE:
+            break;
+          case TK_NUM:
+            tokens[nr_token].type = rules[i].token_type;
+            for (int index = 0; index < substr_len; index++)
+            {
+              tokens[nr_token].str[index] = *(substr_start + index);
+            }
+            nr_token++;
+            break;
+          default:
+            tokens[nr_token].type = rules[i].token_type;
+            nr_token++;
+            break;
+        } 
         break;
       }
     }
@@ -107,19 +126,120 @@ static bool make_token(char *e) {
       return false;
     }
   }
-
+  nr_token--;
   return true;
+}
+
+static bool check_parentheses(int p, int q){
+  bool check_par_flag = true;
+  int cnt = 0;
+  while (p < q){
+    if (tokens[p].type == TK_PARL) cnt++;
+    else if (tokens[p].type == TK_PARR) cnt--;
+    if (cnt == 0) check_par_flag = false;
+    if (cnt < 0) legal_flag = 0; 
+    p++;
+  }
+  if (tokens[p].type == TK_PARR) cnt--;
+  if (cnt != 0)
+  {
+    check_par_flag = false;
+    legal_flag = 0;
+  }
+  return check_par_flag;
+}
+
+word_t eval(int p, int q){
+  if (legal_flag == 0) return 0;
+  if (p > q)
+  {
+    legal_flag = 0;
+    return 0;
+  }
+  else if (p == q)
+  {
+    if(tokens[p].type != TK_NUM)
+    {
+      legal_flag = 0;
+      return 0;
+    }
+    else
+    {
+      int substr_len = strlen(tokens[p].str);
+      word_t value = 0;
+      for (int index = 0; index < substr_len; index++)
+      {
+        value = value * 10 + (tokens[p].str[index] - '0');
+      }
+      return value; 
+    }
+  }
+  else if (check_parentheses(p, q) == 1)
+  {
+    return eval(p + 1, q - 1);
+  }
+  else
+  {
+    int op = p, index = p;
+    int in_par = 0;
+    while (index <= q)
+    {
+      if (tokens[index].type == TK_PARL) in_par++;
+      else if (tokens[index].type == TK_PARR) in_par--;
+      
+      if (in_par == 0)
+      {
+        if ((tokens[index].type == TK_PLUS) || (tokens[index].type == TK_MINUS))
+        {
+          op = index;
+        }
+        else if ((tokens[index].type == TK_MULTIPLY) || (tokens[index].type == TK_DIVIDE))
+        {
+          if ((tokens[op].type != TK_PLUS) && (tokens[op].type != TK_MINUS)) op = index;
+        }
+      }
+      index++;
+    }
+    
+    word_t val1 = eval(p, op - 1);
+    word_t val2 = eval(op + 1, q);
+
+    switch (tokens[op].type)
+    {
+    case TK_PLUS:
+      return val1 + val2;
+    case TK_MINUS:
+      return val1 - val2;
+    case TK_MULTIPLY:
+      return val1 * val2;
+    case TK_DIVIDE:
+      return val1 / val2;
+    default:
+      legal_flag = 0;
+      return 0;
+    }
+  }
 }
 
 
 word_t expr(char *e, bool *success) {
+  
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
+  *success = true;
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  legal_flag = 1;
+  word_t result = 0;
+  result = eval(0, nr_token);
 
-  return 0;
+  if (legal_flag == 0)
+  {
+    printf("Illegal expression!\n");
+    return 0;
+  }
+
+  return result;
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -14,6 +14,7 @@
 ***************************************************************************************/
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -23,6 +24,8 @@
 // this should be enough
 static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
+static int pointer = 0;
+static bool overflow_flag = 0;
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
@@ -31,8 +34,119 @@ static char *code_format =
 "  return 0; "
 "}";
 
+static uint32_t choose(uint32_t n){
+  return rand() % n;
+}
+
+static void buf_clean(){
+  for (int i = 0; i < pointer; i++)
+  {
+    buf[i] = '\0';
+  }
+  pointer = 0;
+}
+
+static bool check_overflow(int pointer, uint32_t substr_len){
+  if (pointer + substr_len > 65534) return 1;
+  return 0;
+}
+
+static void gen_num(){
+  if (overflow_flag == 1) return;
+  uint32_t substr_len = choose(9); 
+  overflow_flag = check_overflow(pointer, substr_len + 1);
+  int ascii_zero = (int)'0';
+  if (overflow_flag == 0)
+  {
+    if (substr_len == 0)
+    {
+      buf[pointer++] = (char)(choose(10) + ascii_zero);
+    }
+    else
+    {
+      buf[pointer++] = (char)(choose(9) + 1 + ascii_zero);
+      for (uint32_t i = 1; i <= substr_len; i++)
+      {
+        buf[pointer++] = (char)(choose(10) + ascii_zero);
+      }  
+    }
+  }
+}
+
+static void gen_parl(){
+  if (overflow_flag == 1) return; 
+  overflow_flag = check_overflow(pointer, 1);
+  if (overflow_flag == 0)
+  {
+    buf[pointer++] = '(';
+  }
+}
+
+static void gen_parr(){
+  if (overflow_flag == 1) return; 
+  overflow_flag = check_overflow(pointer, 1);
+  if (overflow_flag == 0)
+  {
+    buf[pointer++] = ')';
+  }
+}
+
+static void gen_rand_op(){
+  if (overflow_flag == 1) return; 
+  overflow_flag = check_overflow(pointer, 1);
+  if (overflow_flag == 0)
+  {
+    switch (choose(4))
+    {
+      case 0:
+        buf[pointer++] = '+';
+        break;
+      case 1:
+        buf[pointer++] = '-';
+        break;
+      case 2:
+        buf[pointer++] = '*';
+        break;  
+      default:
+        buf[pointer++] = '/';
+        break;
+    }
+  }
+}
+
+static void gen_space(){
+  if (overflow_flag == 1) return; 
+  uint32_t space_num = choose(4);
+  overflow_flag = check_overflow(pointer, space_num);
+  if (overflow_flag == 0)
+  {
+    for (int i = 0; i < space_num; i++)
+    {
+      buf[pointer++] = ' ';
+    }
+  }
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  if (overflow_flag == 1) return;
+  gen_space();
+  switch (choose(3))
+  {
+  case 0:
+    gen_num();
+    break;
+  case 1:
+    gen_parl();
+    gen_rand_expr();
+    gen_parr();
+    break;
+  default:
+    gen_rand_expr();
+    gen_rand_op();
+    gen_rand_expr();
+    break;
+  }
+  gen_space();
 }
 
 int main(int argc, char *argv[]) {
@@ -42,27 +156,40 @@ int main(int argc, char *argv[]) {
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
-  int i;
+  
+  FILE *fp = NULL;
+  int i = 0;
+  int result = 0;
+  
   for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+    int ret = 1;
+    while (ret != 0)
+    {
+      buf_clean();
+      gen_rand_expr();
+      while (overflow_flag == 1)
+      {
+        overflow_flag = 0;
+        buf_clean();
+        gen_rand_expr();
+      }
+      buf[pointer] = '\0';
+      
+      sprintf(code_buf, code_format, buf);
 
-    sprintf(code_buf, code_format, buf);
+      FILE *fp = fopen("/tmp/.code.c", "w");
+      assert(fp != NULL);
+      fputs(code_buf, fp);
+      fclose(fp);
 
-    FILE *fp = fopen("/tmp/.code.c", "w");
-    assert(fp != NULL);
-    fputs(code_buf, fp);
-    fclose(fp);
-
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
-
+      ret = system("gcc -Wall -Werror /tmp/.code.c -o /tmp/.expr >/dev/null 2>&1");
+    }
+    
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
 
-    int result;
     ret = fscanf(fp, "%d", &result);
     pclose(fp);
-
     printf("%u %s\n", result, buf);
   }
   return 0;
